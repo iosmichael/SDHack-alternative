716ca5cb9ae83425070d600d0bae16b54beb8f4b
Sakura Rapolu <rapolusakura@gmail.com>
2018-10-14 02:24:31 -0700

    created envelope not sending it

--- SDHack/Controllers/ModalViewController.swift
+++ SDHack/Controllers/ModalViewController.swift
@@ -27,15 +27,12 @@ class ModalViewController: UIViewController {
             "lastName": lastName.text!,
             "email": email.text!
         ]
+        
         self.navigationController?.pushViewController(controller, animated: true)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func createEnvelope() -> String {
-        
     }
     
     
--- SDHack/Model/DocuSignAPI.swift
+++ SDHack/Model/DocuSignAPI.swift
@@ -19,10 +19,10 @@ class DocuSignAPI: NSObject {
         let sellerDict = ["email":"rapolusakura@gmail.com","name":"Sakura Rapolu","roleName":"Seller"]
         let buyerDict = ["email":"x4liu@eng.ucsd.edu","name":"Michael","roleName":"Buyer"]
         guard let url = URL(string: createEnvelopeApi) else {return assertionFailure("URL Failed")}
-        let parameters = ["status":"sent","templateId":"3094dff1-06e9-4fc3-b6b9-ecda701d4554","templateRoles":[sellerDict,buyerDict]]  as [String : Any]
+        let parameters = ["status":"created","templateId":"3094dff1-06e9-4fc3-b6b9-ecda701d4554","templateRoles":[sellerDict,buyerDict]]  as [String : Any]
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.addValue("Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAAZ70PZjHWSAgAAKfgHakx1kgCAIkI7G6XMQdIiPxoo8qM80QVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MACAIp2JXTHWSA.TATf5RjK6PabLCwU1J7NZfDFqpH2gQlCP9KGFBdqbv4-5JD4DUuh-k4xe9TL6qKLMCx2mpe8mxPhgan0ndJK7nnJC3AXtI4L5u-KKOIRaMWtrejUVbE4dpk324vuGqyikGXtwwMCIKknL_TSSEgRcsE2kZmYXsdi4i1T3iu_TYIuLXX9FTrDHbOVU8FH0YONT0zHgFS4n8dn30cCCVHoqsbksND4jSlsNWN7nTKTKiRH0xcTOlR35C28OIipTxkbRdn2QveUSirAWCZPiUjokNnwUID1_6Ex4bWKqRbxz8SEwhHDly4hK_j86U_fIdLBfMp6z5dV5sq1jRYU_uyOsA", forHTTPHeaderField: "Authorization")
+        request.addValue("Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwCA4FowqTHWSAgAgCB-Puwx1kgCAIkI7G6XMQdIiPxoo8qM80QVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MACAXQisqDHWSA.uSjsPJEtZWnqgOzsbD7YfGfRrOzCLaFxKhCJoQNMo3RXneKUj9t0V7ZnMCPxW6ZWOmJ28KW0hUyMLWWtOJ4VWNXAfmmIt5JUCpM_ZT9r6LwOcMkt70JamJEECZaUpqLx65h7qDWC5483PWrz_D6U6ZzjMznFVyAxJifEJ6IpLtKrySigvTjbBsIshPvNMThqVg6VJoe6lm3MihT_owQ3YiAkjg4lGTo59bmrpNKsn9_rcu9TQKyI9swYUQUiiz17VEiyhY5LgN7POGmGknk3qLCw6tGZhIgESF0U2MMiL7Zcu-xBmc9EVSsDOACMBP0NzfpSqk7YoE5CXoV2iPfGpg", forHTTPHeaderField: "Authorization")
         request.addValue("application/json", forHTTPHeaderField:"Content-Type")
         do { request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) } catch {print("HELLO")}
         
