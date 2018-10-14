860ab2934266dd660b648271b9f46e18c41da0b9
Sakura Rapolu <rapolusakura@gmail.com>
2018-10-13 18:21:53 -0700

    got the api to work! need rest api

--- SDHack/ContractDetailsViewController.swift
+++ SDHack/ContractDetailsViewController.swift
@@ -47,7 +47,7 @@ class ContractDetailsViewController: UIViewController, UITableViewDelegate, UITa
         guard let url = URL(string: apiToContact) else {return assertionFailure("URL Failed")}
         
         var request = URLRequest(url: url)
-        request.setValue("Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQkAAAABAAUABwCAYT9FIDHWSAgAgKFiU2Mx1kgCAIkI7G6XMQdIiPxoo8qM80QVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4EgABAAAACwAAAGludGVyYWN0aXZlMACANA5EIDHWSA.qAhB6vcx3mqeA4Se-UfaVAQiRaoX_C48eYs59B879dPCXQerO9__EURHuFCXc1cRtCgYdKkCZcRIUloVWAtewUv2Juu0VKZRK2X0JKKPEiGaPpL1xpxkVOb6ncTiWaN9U-qZjgyLQHsqIitWJn2x3YpGiOsf1Y1D3USjVfiHT7bqFCTVOv3AHqtdMX28SXRE3OZ8f3T7VKPnaPOzgwr-SuCigGHLIFN_aPx-7_MqOC_D1G6C2Y0W4y8UGuKwMp0iceWwuzWVWTdBjvLx8nLWSZ9eTq13GgHkeOsi9NxHCnag25I5AK4RxLt8jgwTUIx5WewcBcDaB9MvMKXAYoJYGw", forHTTPHeaderField: "Authorization")
+        request.setValue("Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAAZ70PZjHWSAgAAKfgHakx1kgCAIkI7G6XMQdIiPxoo8qM80QVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MACAIp2JXTHWSA.TATf5RjK6PabLCwU1J7NZfDFqpH2gQlCP9KGFBdqbv4-5JD4DUuh-k4xe9TL6qKLMCx2mpe8mxPhgan0ndJK7nnJC3AXtI4L5u-KKOIRaMWtrejUVbE4dpk324vuGqyikGXtwwMCIKknL_TSSEgRcsE2kZmYXsdi4i1T3iu_TYIuLXX9FTrDHbOVU8FH0YONT0zHgFS4n8dn30cCCVHoqsbksND4jSlsNWN7nTKTKiRH0xcTOlR35C28OIipTxkbRdn2QveUSirAWCZPiUjokNnwUID1_6Ex4bWKqRbxz8SEwhHDly4hK_j86U_fIdLBfMp6z5dV5sq1jRYU_uyOsA", forHTTPHeaderField: "Authorization")
 
         Alamofire.request(request).validate().responseJSON() { response in
             print(response)
--- SDHack/CreateContractViewController.swift
+++ SDHack/CreateContractViewController.swift
@@ -14,7 +14,28 @@ class CreateContractViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let accountId = "5cd608e8-b842-4b35-90c8-8b7b1d530ee9"
+////        //let apiToContact = "https://demo.docusign.net/v2/accounts/\(accountId)/templates"
+////        //let parameters = ["url":link]
+////        Alamofire.request(apiToContact, method: .get, encoding: JSONEncoding.default, headers: nil).responseString {JSON in
+////            print(JSON)
+////        }
+////
+        let apiToContact = "https://demo.docusign.net/restapi/v2/accounts/5cd608e8-b842-4b35-90c8-8b7b1d530ee9/templates"
+
+        guard let url = URL(string: apiToContact) else {return assertionFailure("URL Failed")}
+
+        var request = URLRequest(url: url)
+        request.setValue("Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAAZ70PZjHWSAgAAKfgHakx1kgCAIkI7G6XMQdIiPxoo8qM80QVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MACAIp2JXTHWSA.TATf5RjK6PabLCwU1J7NZfDFqpH2gQlCP9KGFBdqbv4-5JD4DUuh-k4xe9TL6qKLMCx2mpe8mxPhgan0ndJK7nnJC3AXtI4L5u-KKOIRaMWtrejUVbE4dpk324vuGqyikGXtwwMCIKknL_TSSEgRcsE2kZmYXsdi4i1T3iu_TYIuLXX9FTrDHbOVU8FH0YONT0zHgFS4n8dn30cCCVHoqsbksND4jSlsNWN7nTKTKiRH0xcTOlR35C28OIipTxkbRdn2QveUSirAWCZPiUjokNnwUID1_6Ex4bWKqRbxz8SEwhHDly4hK_j86U_fIdLBfMp6z5dV5sq1jRYU_uyOsA", forHTTPHeaderField: "Authorization")
+
+        Alamofire.request(request).validate().responseJSON() { response in
+            print(response)
+        }
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
     
     override func didReceiveMemoryWarning() {
@@ -22,7 +43,6 @@ class CreateContractViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
 }
 
 
